72cffe4dbe1815c916267f4bec3fdf396a3072e0
Rifky <rfqgal@gmail.com>
2024-06-17 21:41:11 +0700

    fix: output

--- utils/format_output.py
+++ utils/format_output.py
@@ -20,8 +20,10 @@ def format_image_pred(pred, classes):
     top_2_prob = pred[0][top_2_ind]
     top_2_classes = np.array(classes)[top_2_ind]
     for index, key in enumerate(top_2_classes):
-        # output["top_2"].append({key: top_2_prob[index].item()})
         output["top_2"][key] = top_2_prob[index].item()
     output = ImagePred(classes=output['classes'], prob=output['prob'], top_2=output["top_2"])
-    print(output)
-    return output
+    return {
+        status: "success",
+        message: "Predict success",
+        data: output,
+    }
